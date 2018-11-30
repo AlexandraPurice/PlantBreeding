329bf6af9bee9c93d50735663a1119eb9a491c2f
PuriceAlexandra <aalexaandraa497@gmail.com>
2018-11-30 19:12:33 +0200

    made table a generic component, deletet field class, added table scss

--- ClientApp/src/app/pages/table/field.ts
+++
@@ -1,9 +0,0 @@
-export class Field {
-  constructor(
-    public id: number,
-    public title: string,
-    public size: string,
-    public upload: string,
-    public result: string,
-  ) { }
-}
--- ClientApp/src/app/pages/table/table.component.ts
+++ ClientApp/src/app/pages/table/table.component.ts
@@ -1,22 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-import { Field } from './field';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
-  styleUrls: ['./table.component.scss']
+  styleUrls: ['../../shared/shared.module.scss',
+'./table.component.scss']
 })
 
 export class TableComponent implements OnInit {
-  fields = [
-    new Field( 1, 'File1', '2', '22.10.2018', 'result'),
-    new Field( 2, 'File2', '2', '22.10.2018', 'result'),
-    new Field( 3, 'File3', '2', '22.10.2018', 'result'),
-    new Field( 4, 'File4', '2', '22.10.2018', 'result'),
-    new Field( 5, 'File5', '2', '22.10.2018', 'result'),
-  ];
 
-  header = ['No.', 'File', 'Size', 'File Upload Date', 'Result'];
+  @Input() headings;
+  @Input() data;
 
   constructor() { }
 
