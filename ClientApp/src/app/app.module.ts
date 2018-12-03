a4964682032d877ff136ee8e88a60d64b9bd79db
Morosanu Vladut <vladmorosanu97@gmail.com>
2018-12-03 20:11:37 +0200

    resolved but at "upload genoms" page

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { FormComponent } from './pages/upload-genoms/add-genom/form.component';
 import { SharedModule } from './shared/shared.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
@@ -6,12 +7,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NotFoundComponent } from './security/not-found/not-found.component';
 import { ViewGenomsComponent } from './pages/view-genoms/view-genoms.component';
+import { UploadGenomsComponent } from './pages/upload-genoms/upload-genoms.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NotFoundComponent,
     ViewGenomsComponent,
+    UploadGenomsComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
