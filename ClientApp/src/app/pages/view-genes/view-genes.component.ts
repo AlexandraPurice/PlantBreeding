d3180e3136f95417cefaaf53f7ea09be9f8e127a
Vlad Morosanu <vladmorosanu97@gmail.com>
2018-12-18 14:30:14 +0200

    refactoring app

--- ClientApp/src/app/pages/view-genes/view-genes.component.ts
+++ ClientApp/src/app/pages/view-genes/view-genes.component.ts
@@ -7,25 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewGenesComponent implements OnInit {
   headings = [
-    { key: 'name', label: 'Name', width: '25%'},
-    { key: 'length', label: 'Length', width: '15%'},
-    { key: 'proteinDescription', label: 'Protein description', width: '25%'},
-    { key: 'graph', label: 'Graph', width: '15%' },
+    { key: 'name', label: 'Name', width: '25%' },
+    { key: 'length', label: 'Length', width: '15%' },
+    { key: 'proteinDescription', label: 'Protein description', width: '25%' },
     { key: 'effects', label: 'Effects', width: '15%', link: './view-effects' }
-
   ];
 
   dataTable = [
-    {name: 'Name 1', length: '92',  proteinDescription: 'signam peptidase subunit-12', graph: 'View graph', effects: 'View effects'},
-    {name: 'Name 2', length: '169',  proteinDescription: 'tpa: protein kinase', graph: 'View graph', effects: 'View effects'},
-    {name: 'Name 3', length: '614',  proteinDescription: 'protein scai-like', graph: 'View graph', effects: 'View effects'},
-    {name: 'Name 4', length: '368',  proteinDescription: 'kelc repeat-containing f-box ', graph: 'View graph', effects: 'View effects'},
-    {name: 'Name 5', length: '296',  proteinDescription: 'homeodomain-like protein', graph: 'View graph', effects: 'View effects'}
+    { name: 'Name 1', length: '92', proteinDescription: 'signam peptidase subunit-12', effects: 'View effects' },
+    { name: 'Name 2', length: '169', proteinDescription: 'tpa: protein kinase', effects: 'View effects' },
+    { name: 'Name 3', length: '614', proteinDescription: 'protein scai-like', effects: 'View effects' },
+    { name: 'Name 4', length: '368', proteinDescription: 'kelc repeat-containing f-box ', effects: 'View effects' },
+    { name: 'Name 5', length: '296', proteinDescription: 'homeodomain-like protein', effects: 'View effects' }
   ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
--- ClientApp/src/app/services/gene.service.ts
+++ ClientApp/src/app/services/gene.service.ts
@@ -1,9 +1,13 @@
+import { EffectModel } from './../shared/models/effect-model';
 import { Injectable } from '@angular/core';
+import { GeneModel } from '../shared/models/gene-model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GeneService {
 
+  genes: Array<GeneModel>;
+  effects: Array<EffectModel>;
   constructor() { }
 }
